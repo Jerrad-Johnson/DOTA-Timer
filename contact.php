8e30b97ea0da0a99f7585e98a8622598cfb98056
Jerrad Johnson <j_johnson21@mail.fhsu.edu>
2019-09-12 09:39:29 -0600

    Contact page created

---
+++ contact.php
@@ -0,0 +1,26 @@
+<?php
+include 'includes/header.php'
+?>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <p class="header">DOTA Timer</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col"></div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non sodales neque sodales ut etiam sit amet. Id aliquet lectus proin nibh nisl condimentum id. Posuere urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus. Purus ut faucibus pulvinar elementum integer enim neque volutpat. Id donec ultrices tincidunt arcu non sodales neque. Justo nec ultrices dui sapien eget mi proin sed libero. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Dignissim enim sit amet venenatis. Integer enim neque volutpat ac. Sed faucibus turpis in eu mi bibendum neque egestas congue. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Quis vel eros donec ac odio tempor orci dapibus ultrices. Magna fringilla urna porttitor rhoncus. Feugiat sed lectus vestibulum mattis. Ultricies integer quis auctor elit sed vulputate. Dui accumsan sit amet nulla facilisi morbi. Penatibus et magnis dis parturient montes nascetur ridiculus mus. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin.</p>
+
+        <p>Quam viverra orci sagittis eu volutpat odio facilisis. Ornare arcu dui vivamus arcu. Egestas diam in arcu cursus euismod quis viverra nibh cras. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Semper quis lectus nulla at. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Vel risus commodo viverra maecenas accumsan lacus vel. Mauris in aliquam sem fringilla ut morbi. Elementum eu facilisis sed odio.</p>
+
+        <p>Cras adipiscing enim eu turpis egestas pretium. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Quis viverra nibh cras pulvinar mattis. Felis donec et odio pellentesque. Dolor purus non enim praesent elementum facilisis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Tortor pretium viverra suspendisse potenti nullam ac. Nisi vitae suscipit tellus mauris a. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Arcu vitae elementum curabitur vitae. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum.</p>
+    </div>
+</div>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
