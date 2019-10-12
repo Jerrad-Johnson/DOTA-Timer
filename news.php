5d5610ceba46178cc0e91dd6b98a3628eb0fb682
Jerrad Johnson <j_johnson21@mail.fhsu.edu>
2019-10-11 18:24:33 -0600

    News page -- Content added, UI styled.

--- news.php
+++ news.php
@@ -3,26 +3,24 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12">
-                <p class="header">DOTA Timer</p>
+                <p class="header">News</p>
             </div>
         </div>
 
             <?php
                 if ($f1 = fopen('sources/newsHeadline.txt', 'r')){
+                    $i = 0;
                     while (!feof($f1)) {
                         $lineHeader = fgets($f1);
                         echo "<div class=\"row\"> <div class=\"col newsHeadline\">" . strtoupper($lineHeader) . "</div> </div>";
 
-                        if ($f2 = fopen('sources/newsContent.txt', 'r')) {
-                            while (!feof($f2)) {
-                                $lineContent = fgets($f2);
-                                echo "<div class=\"row\"> <div class=\"col newsContent\">" . $lineContent . "</div> </div>";
-                            }
-                        }
+                        $contentRead = "sources/newsContent.txt";
+                        $lineContent = file($contentRead);
+                        echo "<div class=\"row\"> <div class=\"col newsContent\">" . $lineContent[$i] . "</div> </div>";
 
+                        $i == $i++;
                     }
                 fclose($f1);
-                fclose($f2);
                 }
             ?>
     </div>
