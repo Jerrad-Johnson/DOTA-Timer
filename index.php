4f1ab3fec9d1b0f482b3eec4c035016834e3878e
Jerrad Johnson <j_johnson21@mail.fhsu.edu>
2019-10-13 19:31:24 -0600

    Home Page -- Content edited, CSS added.

--- index.php
+++ index.php
@@ -3,17 +3,17 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12">
-                <p class="header">DOTA Timer</p>
+                <p class="header">Home Page</p>
             </div>
         </div>
 
         <div class="row">
-            <div class="col">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Arcu non sodales neque sodales ut etiam sit amet. Id aliquet lectus proin nibh nisl condimentum id. Posuere urna nec tincidunt praesent semper feugiat nibh sed pulvinar. Dui sapien eget mi proin sed libero enim sed faucibus. Purus ut faucibus pulvinar elementum integer enim neque volutpat. Id donec ultrices tincidunt arcu non sodales neque. Justo nec ultrices dui sapien eget mi proin sed libero. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Dignissim enim sit amet venenatis. Integer enim neque volutpat ac. Sed faucibus turpis in eu mi bibendum neque egestas congue. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Quis vel eros donec ac odio tempor orci dapibus ultrices. Magna fringilla urna porttitor rhoncus. Feugiat sed lectus vestibulum mattis. Ultricies integer quis auctor elit sed vulputate. Dui accumsan sit amet nulla facilisi morbi. Penatibus et magnis dis parturient montes nascetur ridiculus mus. Id cursus metus aliquam eleifend mi in nulla posuere sollicitudin.</p>
+            <div class="col homepage">
+                <p>Welcome! The purpose of this website is to provide you with a valuable aid that will give you an advantage in the immensely popular computer game "DOTA 2." We'll achieve this by teaching you how to use our timer, which is a very simple device. Just press "start" at the right time, and listen for the alerts that will inform you when to take action.</p>
 
-                <p>Quam viverra orci sagittis eu volutpat odio facilisis. Ornare arcu dui vivamus arcu. Egestas diam in arcu cursus euismod quis viverra nibh cras. Malesuada bibendum arcu vitae elementum curabitur vitae nunc. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Semper quis lectus nulla at. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Vel risus commodo viverra maecenas accumsan lacus vel. Mauris in aliquam sem fringilla ut morbi. Elementum eu facilisis sed odio.</p>
+                <p>There's an instruction page that will teach you how to use the timer and explain its value. If you're very tech savvy, you can jump to the timer page. If you've been playing DOTA for very long, the timer's purpose should be very obvious: it will remind you to check runes, kill Roshan, and so on.</p>
 
-                <p>Cras adipiscing enim eu turpis egestas pretium. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Quis viverra nibh cras pulvinar mattis. Felis donec et odio pellentesque. Dolor purus non enim praesent elementum facilisis. Facilisi etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Tortor pretium viverra suspendisse potenti nullam ac. Nisi vitae suscipit tellus mauris a. Metus dictum at tempor commodo ullamcorper a lacus vestibulum sed. Arcu vitae elementum curabitur vitae. Faucibus interdum posuere lorem ipsum dolor sit amet consectetur adipiscing. Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum.</p>
+                <p>If you browse the rest of this website, you'll see that I also offer miniature news updates which help to keep you informed about what's going on in the DOTA world. And if you love the timer and you're feeling generous, or if you have any comments or requests to send my way, please see the contact page. </p>
             </div>
         </div>
     </div>
