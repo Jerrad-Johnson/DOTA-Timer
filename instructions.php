e0681c61febf67f5d25ddc1d7018f3f1bbef2a38
Jerrad Johnson <j_johnson21@mail.fhsu.edu>
2019-10-13 19:37:06 -0600

    Instruction Page -- Content edited, CSS added.

--- instructions.php
+++ instructions.php
@@ -3,17 +3,20 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-12">
-                <p class="header">DOTA Timer</p>
+                <p class="header">Instructions</p>
             </div>
         </div>
 
         <div class="row">
             <div class="col">
-                <p>Instructions:</p>
-                <ul>
+                <p>If you find these instructions difficult to understand, please go to the contact page and tell me which parts I need to rewrite. I hope you find these instructions useful, and that this website is able to improve your gameplay and your enjoyment!</p>
+
+                <hr class="steps"/>
+                <p class="steps">Steps:</p>
+                <ol>
                     <li>Go to the timer page</li>
                     <li>Press Start</li>
-                </ul>
+                </ol>
             </div>
         </div>
     </div>
